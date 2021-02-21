9fc962163df6745d70b959c0611b82d4c49c88b2
Rahul <mrrahul2016@gmail.com>
2021-02-21 23:30:33 +0530

    update sidebar

--- resources/views/admin/advertisement/index.blade.php
+++ resources/views/admin/advertisement/index.blade.php
@@ -2,14 +2,14 @@
     <div class="col s12 m12 l12">
         <div id="responsive-table" class="card card card-default scrollspy">
             <div class="card-content">
-                <h4 class="card-title camel-case">  Products </h4>
+                <h4 class="card-title camel-case">  Advertisement </h4>
                 <div class="row">
                     <div class="col s12">
                         <a
                             class="modal-trigger
                         waves-effect pull-right waves-light btn gradient-45deg-light-blue-cyan z-depth-3 mb-2"
-                            href="{{route('admin.product.create')}}">
-                            Add Product
+                            href="{{route('admin.advertisement.create')}}">
+                            Add Advertisement
                         </a>
                         <table class="responsive-table bordered striped Highlight">
                             <thead class="text-warning">
