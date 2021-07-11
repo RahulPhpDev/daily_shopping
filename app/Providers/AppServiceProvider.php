6335ca69e4d91bf03073f4339c81039f7139178b
Rahul <mrrahul2016@gmail.com>
2021-07-11 18:07:18 +0530

    defautl migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+         Schema::defaultStringLength(191);
         Blade::component('page-head', PageHead::class);
         User::observe(UserObserver::class);
         Product::observe( ProductObserver::class );
